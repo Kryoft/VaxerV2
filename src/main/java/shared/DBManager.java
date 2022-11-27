03757869a38f5a30d24b56bbe92b6c16e1636c4a
dani-caspa <116575615+dani-caspa@users.noreply.github.com>
2022-11-27 14:44:52 +0100

    Update DBManager.java

--- src/main/java/shared/DBManager.java
+++ src/main/java/shared/DBManager.java
@@ -1,20 +1,28 @@
 package shared;
 
+import centrivaccinali.IndirizzoComposto;
+import centrivaccinali.StruttureVaccinali;
+import cittadini.Vaccinati;
+import org.postgresql.util.PSQLException;
+
 import java.sql.*;
 
 public class DBManager {
     public static void main(String[] args) throws SQLException {
-        //TODO:decidere se mettere i controlli esistecentro(), controlloid(), ControllaCF(), ControllaLogin() per cittadino su db e come gestire eccezioni
-        //String CercaCentro = "SELECT * FROM Centro";
-        String InsVaccinato ="INSERT INTO Vaccinato\n" +
-        "VALUES('CSPDNL01M11I577Q','Daniele','Caspani','11/10/2001','31500','Moderna','3')";
-        UpData(InsVaccinato);
-        String InsIscritto ="INSERT INTO Iscritto\n" +
-                "VALUES('danielec1108@gmail.com','Dani','1234','CSPDNL01M11I577W')";
-        UpData(InsVaccinato);
-        String InsEvento ="INSERT INTO Evento\n" +
-                "VALUES('2','Mal di testa','Caspani','4','Lieve')";
-        UpData(InsVaccinato);
+        //TODO:fare query per ricerca per comune e tipologia,nome centro  e visualizzainfo()
+        //TODO:eventi avversi?
+        //TODO:gestire controlloLogin() nel db
+        //TODO: gestire esistecentro() (fare nome centro unique?)
+        //TODO: gestire eccezioni Primary key, Foreign Key(per codice fiscale email), Check Costraint
+        String ControllaIdVax = "SELECT Identificativo,cod_centro  FROM Centro"; // per il controllo id in vaccinato
+        String ControllaIdFisc = "SELECT V.Identificativo,V.Cod_Fiscale  FROM Vaccinato V";// per il controllo id in cittadino
+        String ControllaIdIscritto="";
+        //String InsVaccinato ="INSERT INTO Vaccinato\n" +
+       // "VALUES('CSPDNL01M11I577Q','Daniele','Caspani','11/10/2001','31500','Moderna','3')";
+        //String InsIscritto ="INSERT INTO Iscritto\n" +
+               // "VALUES('danielec1108@gmail.com','Dani','1234','CSPDNL01M11I577W')";
+        InsertEvento(2,"CIAO",3,"");
+       // InsertCentro("Ospedale","Erba", "er", StruttureVaccinali.Tipologia.OSPEDALIERO, IndirizzoComposto.Qualificatore.VIA,"Dei caduti",3,"22036");
     }
 
     public static Connection Connected() throws SQLException {
@@ -54,26 +62,29 @@ public class DBManager {
         st.close();
     }
 
-    public static void InsertCentro(String nome,String comune,String sigla, String tipologia,String qualificatore,String nome_via, int num_civico,String cap) throws SQLException {
-        String InsCentro = "INSERT INTO centrovaccini(nome,comune,sigla,tipologia,qualificatore,nome_via,num_civico,cap)\n" +
-                "VALUES(" + nome + "," + comune + "," + sigla + "," + tipologia + "," + qualificatore + "," + nome_via + ",\n"
-        + num_civico + "," + cap + ")";
+    public static void InsertCentro(String nome, String comune, String sigla, StruttureVaccinali.Tipologia tipologia, IndirizzoComposto.Qualificatore qualificatore, String nome_via, int num_civico, String cap) throws SQLException {
+        String InsCentro = "INSERT INTO centrovaccini(nome,comune,sigla,qualificatore,nome_via,num_civico,cap,Tipologia)\n"
+                + "VALUES(" + "'" + nome + "'" + "," + "'" + comune + "'" + "," + "'" + sigla + "'" + "," + "'" + qualificatore + "'\n"
+                + "," + "'" + nome_via + "'" + "," + "'" + num_civico + "'" + "," + "'" + cap + "'" + "," + "'\n"
+                + tipologia + "'" + ")";
         UpData(InsCentro);
     }
-    public static void InsertVaccinato(String cod_fiscale,String nome,String cognome, String data,int identificativo,String vaccino, String cod_centro) throws SQLException {
+    public static void InsertVaccinato(String cod_fiscale, String nome, String cognome, Date data, int identificativo, Vaccinati.Vaccino vaccino, int cod_centro) throws SQLException {
         String InsVaccinato = "INSERT INTO Vaccinato\n" +
-                "VALUES(" + cod_fiscale + "," + nome + "," + cognome + "," + data + "," + identificativo + "," + vaccino + ",\n"
+                "VALUES(" + "'" + cod_fiscale + "'" +  "," + "'" +  nome + "'" + "," + "'" +  cognome + "'" + "," + "'" + data + "'" + "," + "'\n" +
+                identificativo + "'" + "," + "'" + vaccino + "'" + ",\n"
                 + cod_centro + ")";
         UpData(InsVaccinato);
     }
 
     public static void InsertIscritto(String email,String username,String password, String cod_fiscale) throws SQLException {
-        String InsIscritto = "INSERT INTO Iscritto VALUES(" + email + "," + username + "," + password + "," + cod_fiscale +  ")";
+        String InsIscritto = "INSERT INTO Iscritto VALUES(" + "'" + email + "'" + "," + "'" +  username + "'" + "," + "'" + password + "'" + "," + "'" + cod_fiscale +  "'" + ")";
         UpData(InsIscritto);
     }
 
-    public static void InsertEvento(String cod_centro,String evento,String indice, String note) throws SQLException {
-        String InsEvento = "INSERT INTO Iscritto VALUES(" + cod_centro + "," + evento + "," + indice + "," + note +  ")";
+    public static void InsertEvento(int cod_centro,String evento,int indice, String note) throws SQLException {
+        String InsEvento = "INSERT INTO Evento VALUES(" + "'" + cod_centro + "'" +  "," + "'" + evento + "'" + "," + "'\n"
+                + indice + "'" + "," + "'" +  note + "'" +  ")";
         UpData(InsEvento);
     }
 }
