cf6e5fe1446c7a85aa1a7844a0372809271a2c6d
Kryoft <fulmine.cris@gmail.com>
2023-01-01 18:31:31 +0100

    Integrazione con Maven completata Il file pom.xml è stato modificato in modo che maven sia in grado di connettersi a postgresql, eliminare e ricreare il database "ProgettoB" (ricreandolo eseguendo le query inserite in "src/main/sql/CreaDatabase.sql") e creare il jar per l'esecuzione del programma. Per eliminare i dati delle build precedenti, eseguire un maven clean (con il terminale inserirsi nella cartella dove è presente il file pom.xml ed eseguire 'mvn clean'), mentre per eseguire le operazioni descritte sopra, eseguire un maven install ('mvn install' nel terminale -- 'mvn clean install' permette di farli entrambi uno dopo l'altro)

---
+++ src/main/sql/CreaDatabase.sql
@@ -0,0 +1,65 @@
+create table CENTROVACCINI(
+	Codice SERIAL,
+     Nome varchar(80) NOT NULL,
+	Comune varchar(100),
+	Sigla char(2),
+	Tipologia varchar(15), 
+	Qualificatore varchar(10), 
+	Nome_via varchar(50),
+     	Num_civico smallint,
+    	Cap char(5),
+	primary key (Codice)
+);
+
+create table VACCINATI(
+	Cod_Fiscale char(16),
+	Nome varchar(30),
+	Cognome varchar(30),
+	Data DATE,
+	Identificativo SERIAL PRIMARY KEY,
+	Vaccino varchar(15),
+	Cod_Centro int,
+	foreign key (Cod_Centro) references CENTROVACCINI(Codice)
+		on delete cascade
+		on update cascade
+);
+
+
+create table ISCRITTI(
+	Email varchar(30),
+	Username varchar(20) UNIQUE,
+	Password varchar(30),
+	Cod_Fiscale char(16) primary key
+);
+
+
+
+create table LOG_EVENTI(
+	Cod_Centro Integer,
+	Cod_Fiscale varchar(30),
+	Nome_Evento varchar(80),
+	Indice smallint CHECK(indice between 0 AND 5),
+	Note varchar(256),
+
+	foreign key (Cod_Fiscale) references ISCRITTI(Cod_Fiscale)
+		on update cascade
+		on delete set null,
+
+	foreign key(Cod_Centro) references CentroVaccini(codice)
+		on update cascade
+		on delete no action,
+	
+	Constraint Log UNIQUE(Cod_Fiscale,Nome_Evento)
+);
+
+
+INSERT into centrovaccini(nome,comune,tipologia,sigla,qualificatore,nome_via,num_civico,cap) 
+VALUES ('Mangioni','Lecco','OSPEDALIERO','LC','PIAZZA','Vecchia','12','22022');
+INSERT into centrovaccini(nome,comune,tipologia,sigla,qualificatore,nome_via,num_civico,cap) 
+VALUES ('Valduce','Como','HUB','CO','VIA','Napoli','13','22012');
+INSERT into centrovaccini(nome,comune,tipologia,sigla,qualificatore,nome_via,num_civico,cap) 
+VALUES ('H hospital','Erba','AZIENDALE','ER','VIA','Alserio','12','22036');
+INSERT into centrovaccini(nome,comune,tipologia,sigla,qualificatore,nome_via,num_civico,cap) 
+VALUES ('Manzoni','Lecco','OSPEDALIERO','LC','VIA','Cesarea','10','22022');
+INSERT into centrovaccini(nome,comune,tipologia,sigla,qualificatore,nome_via,num_civico,cap) 
+VALUES ('Pizzeria Pinello','Napoli','AZIENDALE','NA','VIA','spaccaNapoli','10','23422');
